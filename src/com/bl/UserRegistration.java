992cae6f56f00ef2a3199cde5c0f4dd4bd17cb41
captjack-4 <90202533+captjack-4@users.noreply.github.com>
2021-11-30 23:55:59 +0530

    Update UserRegistration.java

--- src/com/bl/UserRegistration.java
+++ src/com/bl/UserRegistration.java
@@ -9,7 +9,7 @@ public class UserRegistration {
 	public void firstNameValidation() {
 		System.out.print("Enter first Name :- "); // User Enters First Name
 		String firstName = scan.nextLine();
-		String nameRegex = "^[A-Z][A-Z a-z]{3,}$"; // Conditions of Entering Last name
+		String nameRegex = "^[A-Z][A-Z a-z]{3,}$"; // Conditions of Entering First name
 		Pattern pattern = Pattern.compile(nameRegex);
 		Matcher matcher = pattern.matcher(firstName);
 		System.out.println("First name pattern matching :- " + matcher.matches());
