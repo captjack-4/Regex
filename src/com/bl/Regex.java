5063894e90b1465c17c0b39d4a6d1744e3dff428
unknown <akshayHVshinde04@gmail.com>
2021-11-30 23:45:44 +0530

    UC1_FirstName

---
+++ src/com/bl/Regex.java
@@ -0,0 +1,8 @@
+package com.bl;
+
+public class Regex {
+	public static void main(String[] args) {
+		UserRegistration userData = new UserRegistration(); // object creation
+		userData.firstNameValidation();
+	}
+}
---
+++ src/com/bl/UserRegistration.java
@@ -0,0 +1,17 @@
+package com.bl;
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class UserRegistration {
+	static Scanner scan = new Scanner(System.in);
+
+	public void firstNameValidation() {
+		System.out.print("Enter Last Name :- "); // User Enters First Name
+		String firstName = scan.nextLine();
+		String nameRegex = "^[A-Z][A-Z a-z]{3,}$"; // Conditions of Entering Last name
+		Pattern pattern = Pattern.compile(nameRegex);
+		Matcher matcher = pattern.matcher(firstName);
+		System.out.println("First name pattern matching :- " + matcher.matches());
+	}
+}
