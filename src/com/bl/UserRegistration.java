9bc1746ca825f00836fe808c30ea8e5a5357c01e
unknown <akshayHVshinde04@gmail.com>
2021-12-01 07:27:40 +0530

    UC4_PhoneNumber

--- src/com/bl/UserRegistration.java
+++ src/com/bl/UserRegistration.java
@@ -6,12 +6,12 @@ import java.util.regex.*;
 public class UserRegistration {
 	static Scanner scan = new Scanner(System.in);
 
-	public void emailIDValidation() {
-		System.out.print("Enter Email Address :- "); // User Enters Email Address
-		String emailID = scan.nextLine();
-		String emailRegex = "^[a-z0-9-._]{1,}+[@][a-z0-9 .]{1,}$"; // Conditions for Email Address
-		Pattern pattern = Pattern.compile(emailRegex);
-		Matcher matcher = pattern.matcher(emailID);
+	public void phoneNoValidation() {
+		System.out.print("Enter Phone Number :- "); // User Enters Phone Number
+		String phoneNo = scan.nextLine();
+		String phoneRegex = "^[0-9]{2}(\\s){1}[0-9]{10}$"; // Conditions for Email Address
+		Pattern pattern = Pattern.compile(phoneRegex);
+		Matcher matcher = pattern.matcher(phoneNo);
 		System.out.println("First name pattern matching :- " + matcher.matches());
 	}
 }
