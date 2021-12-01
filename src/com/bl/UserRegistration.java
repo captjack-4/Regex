cb199005aadc5db42ca6aac9885f3a7a9aaa69a3
captjack-4 <90202533+captjack-4@users.noreply.github.com>
2021-12-01 07:47:13 +0530

    Update UserRegistration.java

--- src/com/bl/UserRegistration.java
+++ src/com/bl/UserRegistration.java
@@ -9,7 +9,7 @@ public class UserRegistration {
 	public void passwordValidation() {
 		System.out.print("Enter Password :- "); // User Enters Password
 		String password = scan.nextLine();
-		String passRegex = "^[A-Za-z0-9]{8,}"; // Conditions for Email Address
+		String passRegex = "^[A-Za-z0-9]{8,}"; // Rules for password - There will be minimum 8 Charaters
 		Pattern pattern = Pattern.compile(passRegex);
 		Matcher matcher = pattern.matcher(password);
 		System.out.println("First name pattern matching :- " + matcher.matches());
