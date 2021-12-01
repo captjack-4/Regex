0acb49db415d05c7e7db826b4e8548f4820ce1a2
unknown <akshayHVshinde04@gmail.com>
2021-12-01 13:45:34 +0530

    UC8_PasswordRule4

--- src/com/bl/UserRegistration.java
+++ src/com/bl/UserRegistration.java
@@ -9,8 +9,7 @@ public class UserRegistration {
 	public void passwordValidation() {
 		System.out.print("Enter Password :- "); // User Enters Password
 		String password = scan.nextLine();
-		String passRegex = "^[A-Z{1,}0-9{1,}a-z]{8}$"; // Rules for Password - At least one Upper Case and Numeric
-														// Character
+		String passRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$&_-]){1}([A-Za-z0-9]).{8,}"; // Rules for Password - At least one Upper Case, Numeric Character and Special Character
 		Pattern pattern = Pattern.compile(passRegex);
 		Matcher matcher = pattern.matcher(password);
 		System.out.println("First name pattern matching :- " + matcher.matches());
