d1a87e47e0784ad0b75fbfdbc6e846ddb87952f7
unknown <akshayHVshinde04@gmail.com>
2021-12-01 07:34:34 +0530

    UC5_PasswordRule1

--- src/com/bl/Regex.java
+++ src/com/bl/Regex.java
@@ -3,6 +3,6 @@ package com.bl;
 public class Regex {
 	public static void main(String[] args) {
 		UserRegistration userData = new UserRegistration(); // object creation
-		userData.phoneNoValidation();
+		userData.passwordValidation();
 	}
 }
--- src/com/bl/UserRegistration.java
+++ src/com/bl/UserRegistration.java
@@ -6,12 +6,12 @@ import java.util.regex.*;
 public class UserRegistration {
 	static Scanner scan = new Scanner(System.in);
 
-	public void phoneNoValidation() {
-		System.out.print("Enter Phone Number :- "); // User Enters Phone Number
-		String phoneNo = scan.nextLine();
-		String phoneRegex = "^[0-9]{2}(\\s){1}[0-9]{10}$"; // Conditions for Email Address
-		Pattern pattern = Pattern.compile(phoneRegex);
-		Matcher matcher = pattern.matcher(phoneNo);
+	public void passwordValidation() {
+		System.out.print("Enter Password :- "); // User Enters Password
+		String password = scan.nextLine();
+		String passRegex = "^[A-Za-z0-9]{8,}"; // Conditions for Email Address
+		Pattern pattern = Pattern.compile(passRegex);
+		Matcher matcher = pattern.matcher(password);
 		System.out.println("First name pattern matching :- " + matcher.matches());
 	}
 }
